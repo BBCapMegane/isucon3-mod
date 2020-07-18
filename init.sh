4101bf2139d7d4e75f199d16a976e3c25ed79c7b
seiichi.akiba <seiichi.akiba@roxx.co.jp>
2020-07-18 15:33:39 +0900

    index 貼るように  init を追加

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+MYSQL="mysql -u isucon"
+
+$MYSQL <<EOF
+USE isucon;
+ALTER TABLE memos ADD INDEX index_user(user);
+ALTER TABLE memos ADD INDEX index_is_private(is_private);
+EOF
